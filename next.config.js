3ae20d15dfab42da43c0b8f50c037b8fe943d099
VenseChang <vense@shoplineapp.com>
2022-08-25 02:16:16 +0800

    phase 1

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  assetPrefix: isProd ? '/to-do-list' : undefined,
 }
 
 module.exports = nextConfig
