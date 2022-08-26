74cfc966d1c934888980fa1beac21b8b1c2a689c
VenseChang <vense@shoplineapp.com>
2022-08-27 05:07:38 +0800

    v2.0.1

---
+++ next/static/WMpCoU4vTLJNcH94TG6qu/buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/cb1608f2-03ffe3a9c84d2c0d.js","static/chunks/pages/index-2f97c38f7428c0c1.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
